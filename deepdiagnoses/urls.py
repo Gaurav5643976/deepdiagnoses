da1094ec3ea7867dd59d1ef08f3d1c2c04537cc3
[kavinder panwar] <[9643kavinder@gmail.com]>
2019-02-06 16:52:31 +0530

    heroku deploy 2
    Signed-off-by: [kavinder panwar] <[9643kavinder@gmail.com]>

--- deepdiagnoses/urls.py
+++ deepdiagnoses/urls.py
@@ -20,6 +20,9 @@ from . import settings
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +34,4 @@ urlpatterns = [
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
